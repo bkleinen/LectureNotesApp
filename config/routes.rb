33ec536047ba6f1256e134b5d68d850df5f98f91
Bela Kleinen <kleinen@infrastructure.de>
2011-11-07 20:19:26 +0100

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Lecturemethods::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'lecture_notes#index'
 
   # See how all your routes lay out with "rake routes"
 
